2c052bf4a6e3a697baf4e96ff16867d496b054b7
saraAlsayd <roraya2442@gmail.com>
2023-08-13 08:03:41 -0700

    looool

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-     size = len(sys.argv) - 1
-  
+    size = len(sys.argv) - 1
 if size == 0:
-        print('{:d} arguments.'.format(0))
+    print('{:d} arguments.'.format(0))
 elif size == 1:
-        print('{:d} argument:'.format(1))
+    print('{:d} argument:'.format(1))
 else:
-        print('{:d} arguments:'.format(size))
+    print('{:d} arguments:'.format(size))
 for i in range(size):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
