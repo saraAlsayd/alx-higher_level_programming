3f68a04a1bd155074d6ca211ee44ad42e4059e03
saraAlsayd <roraya2442@gmail.com>
2023-08-14 21:19:43 +0000

    kolkol

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-        """Replacing an element of a list.""" 
+ 
         if idx >= 0 and idx < len(my_list):
             my_list[idx] = element
         return (my_list)
