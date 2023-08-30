6f7fbc31d70c967db0bd64560915cb0cd9ff210e
saraAlsayd <roraya2442@gmail.com>
2023-08-30 13:31:23 -0700

    kool

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list:
+            if count < x:
+                print(element, end=" ")
+                count += 1
+            else:
+                break
+        print()
+        return count
+    except:
+        return 0
