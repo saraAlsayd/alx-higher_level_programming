0e49a9e40c13fb697ba9794d8bb911eae738939c
saraAlsayd <roraya2442@gmail.com>
2023-09-11 15:19:55 -0700

    loook

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """define look up fun to return obj"""
+
+
 def lookup(obj):
     """return alist of object"""
     return(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define class to define list"""
+
+
+class MyList(list):
+    """define function to store the list"""
+
+    def print_sorted(self):
+        """print value from my list"""
+        print(store(self))
