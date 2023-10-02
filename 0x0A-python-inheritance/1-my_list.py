e189ac5a8448616c690f2f8baabaca9da9bf0052
saraAlsayd <128824174+saraAlsayd@users.noreply.github.com>
2023-10-02 00:19:37 +0000

    fff

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""define class to define list"""
+"""Defines a class MyList that inherits from list"""
 
 
 class MyList(list):
-    """define function to store the list"""
+    """Class that inherits from list.
 
+    Args:
+        list (list): list to sort in ascending order.
+    """
     def print_sorted(self):
-        """print value from my list"""
-        print(store(self))
+        """Prints a list in ascending order.
+        """
+        list_ = self[:]
+        list_.sort()
+        print(list_)
