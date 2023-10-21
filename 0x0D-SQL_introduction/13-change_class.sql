711ad3aedfa9fe826c743c8d2c542c9ed0e16577
saraAlsayd <roraya2442@gmail.com>
2023-10-21 12:37:29 -0700

    up13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- Removes all records with a score <= 5 in the table second_table
-UPDATE `second_table` SET `score`<= 5 WHERE `second_table` DELETE;
+DELETE FROM `second_table` WHERE `score`<= 5 WHERE `second_table`;
