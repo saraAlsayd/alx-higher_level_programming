b0e493a002254a5d12893ef529d14dc071c5664e
saraAlsayd <roraya2442@gmail.com>
2023-10-21 15:42:08 -0700

    15up

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Select count and grouping
-SELECT score, COUNT(score) AS NUMBER FROM second_table GROUP BR score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP By score ORDER BY score DESC;
