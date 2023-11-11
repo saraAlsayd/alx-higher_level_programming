d50af7c6b763337f3a36297799ff63338314587b
saraAlsayd <roraya2442@gmail.com>
2023-11-11 03:33:34 -0800

    up1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a class square"""
 
+
 class Square:
     """make empty class his name is square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""defins square and Private instance attribute"""
+
+
 class Square:
+    """class name is square and define method int"""
     def __int__(self, size):
+        """define size as a private attribute"""
         self.__size = size
