df2003ea9b676da8b95c6eb8ee57f371d601c66e
saraAlsayd <roraya2442@gmail.com>
2023-11-17 09:37:58 -0800

    up0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Print script
+// Print string “Javascript is amazing”
 
-const myVar = 'JavaScript is amazing';
-console.log(myvar);
+const myVar = 'Javascript is amazing';
+console.log(myVar);
