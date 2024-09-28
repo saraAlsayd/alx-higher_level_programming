20a017b194d3fa25977c5e8de02f089408e498b6
saraAlsayd <roraya2442@gmail.com>
2024-09-28 08:22:02 -0700

    task9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,5 @@
    -- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name, states.name FROM cities
-JOIN states ON cities.states_id=states.id
+JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
